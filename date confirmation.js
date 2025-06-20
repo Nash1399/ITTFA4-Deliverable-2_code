be015a00482403bef25e6f6c38e316e91c6e488a
Nash1399 <munmadzi99@gmail.com>
2025-06-20 17:02:39 +0200

    Added script tag for date confirmation.js at line 62.

--- date confirmation.js
+++ date confirmation.js
@@ -3,10 +3,11 @@ function confirmdate()
 {
     //create the date confirmation dialog 
     const date_selection = confirm("This is the date your reservation shall be place. Are you sure?");
+    //verify user selected date
     
     if(date_selection){
         alert("Your reservation has been place ");
-
+       localStorage.removeItem("cart");
     window.location.href = "Menu Selection.html";
 
 
